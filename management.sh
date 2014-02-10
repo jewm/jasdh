a8693d2eca4603e0c7e2e7680836eb1514671964
jewm <jewm@users.noreply.github.com>
2014-02-10 21:51:27 +0100

    Update management.sh

--- management.sh
+++ management.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 function scriptUpdate {
-
         wget "https://raw.github.com/jewm/jasdh/master/management.sh" -O "management_new_version.sh" -nv
 
         diff -q "management.sh" "management_new_version.sh" 1>/dev/null
@@ -18,6 +17,65 @@ function scriptUpdate {
         fi
 }
 
+function startServer {
+	echo "Starting server..."
+	$APPLICATION_SERVER_START
+	echo "Server started"
+}
+
+function stopServer {
+	echo "Stopping server..."
+	$APPLICATION_SERVER_STOP
+	echo "Server stopped"
+}
+
+function serverStatus {
+	$APPLICATION_SERVER_STATUS
+}
+
+function deploy {
+	echo "Start deployment"
+
+        if [ ! -f "$WAR_PATH$CURRENT_WAR_NAME" ]; then
+        	echo "$CURRENT_WAR_NAME not found"
+        	echo "Deployment aborted"
+        	exit 1;
+        fi
+
+	stopServer
+
+	if [ ! -d $WAR_BACKUP_PATH ]; then
+		mkdir -p $WAR_BACKUP_PATH
+		echo "Backup-folder created"
+	fi
+
+	if [ -f "$WEBAPPS_PATH$FINAL_WAR_NAME" ]; then
+		mv "$WEBAPPS_PATH$FINAL_WAR_NAME" "$WAR_BACKUP_PATH$(date +"%Y-%m-%d-%H:%M").war"
+		echo "Old war saved"
+	fi
+
+	rm -rf "$WEBAPPS_PATH*"
+	echo "Webapps-folder cleared"
+
+	cp "$WAR_PATH$CURRENT_WAR_NAME" "$WEBAPPS_PATH$FINAL_WAR_NAME"
+	echo "New war moved"
+
+	if [ "$2" = "--auto-rollback" ]; then
+		echo "Rollback on failure"
+	fi
+
+	startServer
+
+	if [ ! $APPLICATION_RUNNING ]; then
+		if [ "$2" = "--auto-rollback" ]; then
+			echo "Rollback on failure"
+		fi
+	fi
+
+	echo "Deployment finished"
+}
+
+
 if [ $AUTO_UPDATE_ENABLED = 1 ]; then
 
         scriptUpdate
@@ -25,65 +83,23 @@ fi
 
 case "$1" in
 	start)
-		echo "Starting server..."
-		$APPLICATION_SERVER_START
-		echo "Server started"
+		startServer
 	;;
 
 	stop)
-		echo "Stopping server..."
-		$APPLICATION_SERVER_STOP
-		echo "Server stopped"
+		stopServer
 	;;
 
 	restart)
-		$0 stop
-		$0 start
+		stopServer
+		startServer
 	;;
 
 	status)
-		$APPLICATION_SERVER_STATUS
+		serverStatus
 	;;
 
 	deploy)
-		echo "Start deployment"
-
-		if [ ! -f "$WAR_PATH$CURRENT_WAR_NAME" ]; then
-			echo "$CURRENT_WAR_NAME not found"
-			echo "Deployment aborted"
-			exit 1;
-		fi
-
-		$0 stop
-
-		if [ ! -d $WAR_BACKUP_PATH ]; then
-			mkdir -p $WAR_BACKUP_PATH
-			echo "Backup-folder created"
-		fi
-
-		if [ -f "$WEBAPPS_PATH$FINAL_WAR_NAME" ]; then
-			mv "$WEBAPPS_PATH$FINAL_WAR_NAME" "$WAR_BACKUP_PATH$(date +"%Y-%m-%d-%H:%M").war"
-			echo "Old war saved"
-		fi
-
-		rm -rf "$WEBAPPS_PATH*"
-		echo "Webapps-folder cleared"
-
-		cp "$WAR_PATH$CURRENT_WAR_NAME" "$WEBAPPS_PATH$FINAL_WAR_NAME"
-		echo "New war moved"
-
-		if [ "$2" = "--auto-rollback" ]; then
-			echo "Rollback on failure"
-		fi
-
-		$0 start
-
-		if [ ! $APPLICATION_RUNNING ]; then
-			if [ "$2" = "--auto-rollback" ]; then
-                        	echo "Rollback on failure"
-                	fi
-		fi
-
-		echo "Deployment finished"
+		deploy
 	;;
 esac
